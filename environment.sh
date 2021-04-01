78badd28421adf3ec2a76f3778769eb59915dcd4
hsuchaochun <leohsu210200@gmail.com>
2021-04-01 18:57:00 +0800

    docker init

---
+++ environment.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+source /opt/ros/melodic/setup.bash
+source ~/catkin_ws/devel/setup.bash
+
+export ROS_IP=192.168.50.11
+export ROS_MASTER_URI=http://192.168.50.11:11311
+
+echo "xavier environment"
